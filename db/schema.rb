575019895376863e196910c73980abc88f48a8e2
Troy Coll <troy.coll@salesforce.com>
2022-01-25 16:32:21 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_01_10_211853) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
